400712924ebdc84450652cf8fd9bd9492ca692bd
syed-git <sdzubair.engg@gmail.com>
2023-05-07 11:45:11 +0530

    updated

--- pages/Swag_Labs_Selectors.js
+++ pages/Swag_Labs_Selectors.js
@@ -1,7 +1,9 @@
 module.exports; swagLabs = {
     'userName': 'input[name="user-name"]',
     'password': 'input[name="password"]',
-    'loginButton': 'input[name="login-button"]'
+    'loginButton': 'input[name="login-button"]',
+    'menuOption': 'id=react-burger-menu-btn',
+    'logout': 'id=logout_sidebar_link'
 }
 
 module.exports = swagLabs;
--- tests/login_test.ts
+++ tests/login_test.ts
@@ -9,3 +9,15 @@ Scenario('Check Invalid Login',  async ({ I }) => {
     await I.clickOn('loginButton')
     await I.see('Epic sadface: Username and password do not match any user in this service')
 });
+
+Scenario('Check Valid Login',  async ({ I }) => {
+    await I.navigateTo('https://www.saucedemo.com/')
+    await I.seeTitleContains('Swag Labs', true)
+    await I.enterText('userName', 'standard_user')
+    await I.enterText('password', 'secret_sauce')
+    await I.clickOn('loginButton')
+    await I.see('Swag Labs')
+    await I.clickOn('menuOption')
+    await I.clickOn('logout')
+    await I.see('Swag Labs')
+});
